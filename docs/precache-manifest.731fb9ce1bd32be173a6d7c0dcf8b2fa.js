4dd07467568be8d6489b31d0b2446f8f8e35a441
Michael Sosa <833@holbertonschool.com>
2020-03-14 07:29:47 -0500

    docs

---
+++ docs/precache-manifest.731fb9ce1bd32be173a6d7c0dcf8b2fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cb05a1a2c7943ca76813bdefd84f95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b22eae91c1394028970",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "197b7cd7d3cdbfb20e26",
+    "url": "/static/css/main.6fe29bac.chunk.css"
+  },
+  {
+    "revision": "0b22eae91c1394028970",
+    "url": "/static/js/2.81c85762.chunk.js"
+  },
+  {
+    "revision": "56a75e4645d1d25a489816a3ff18e1c6",
+    "url": "/static/js/2.81c85762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197b7cd7d3cdbfb20e26",
+    "url": "/static/js/main.8539647b.chunk.js"
+  },
+  {
+    "revision": "e71fbed7cbe4652b87ea",
+    "url": "/static/js/runtime-main.9b9b121a.js"
+  }
+]);
